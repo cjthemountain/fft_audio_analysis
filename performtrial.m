ca16fed05058d5499b9f8fde5d09e8a0beabd567
cjthemountain <toothless@localhost.localdomain>
2015-01-14 17:50:54 -0800

    runtests now queries for filename and autosaves workspace

--- performtrial.m
+++ performtrial.m
@@ -7,9 +7,9 @@ function [tplayedtime,tplayed,trectime,trec,fplayedrange,fplayedamp,frecrange,fr
 	#playedtone = playedtone/3; #scale generated tone amplitude by 1/3 to avoid clipping if necessary
 
 	#play audio through speaker and record output
-	tic; #start a timer
+	#tic; #start a timer
 	recordedtone = aplayrec(playedtone,1,fs,'default');
-	toc; #split the timer
+	#toc; #split the timer
 	recordedtonetime = playedtonetime;
 
 	#convert to frequency domain data
@@ -24,15 +24,11 @@ function [tplayedtime,tplayed,trectime,trec,fplayedrange,fplayedamp,frecrange,fr
 
 	tplayedtime 	= playedtonetime;
 	tplayed 	= playedtone;
-	#fplayedrange	= frequenciesplayed;
-	#fplayedamp	= frequenciesplayedamplitude;
 	fplayedrange	= frequenciesplayed(1:length(frequenciesplayed)/2);
 	fplayedamp	= frequenciesplayedamplitude(1:length(frequenciesplayedamplitude)/2);
 	
 	trectime	= recordedtonetime; 
 	trec		= recordedtone;
-	#frecrange	= frequenciesrecorded;
-	#frecamp		= frequenciesrecordedamplitude;
 	frecrange	= frequenciesrecorded(1:length(frequenciesrecorded)/2);
 	frecamp		= frequenciesrecordedamplitude(1:length(frequenciesrecordedamplitude)/2);
 endfunction
--- runtests.m
+++ runtests.m
@@ -4,23 +4,38 @@ minfreq = 0;	#hz
 fs = 48000;		#sampling frequency
 t = 0.1;
 
+#generate sound wavs
 frequencies = minfreq:(maxfreq-minfreq)/numberoftests:maxfreq;
 trial = cell(numberoftests,8,fs*t);
 
-for i = 1:numberoftests
-	[trial{i,1},trial{i,2},trial{i,3},trial{i,4},trial{i,5},trial{i,6},trial{i,7},trial{i,8}] = performtrial(frequencies(i),fs,t);
+calibrate = input("would you like to run a speaker calibration? ", "s");
+if (strcmp("yes",calibrate)==1)
+	speakerrangetest(fs, trial, 1, .5, .5, .5, 1, .5);
+endif
 
+#prep workspace save to file
+filedesignation = input("filename: ", "s");
+ztime = clock();
+filename = [mat2str(ztime(1)) "-" mat2str(ztime(2)) "-" mat2str(ztime(3))... 
+	    "_" mat2str(ztime(4)) ":" mat2str(ztime(5)) ":" mat2str(floor(ztime(6)))... 
+	    "-" filedesignation ".m"];
+
+#run tests
+for i = 1:numberoftests
+	[trial{i,1},trial{i,2},trial{i,3},trial{i,4},...
+	trial{i,5},trial{i,6},trial{i,7},trial{i,8}] = performtrial(frequencies(i),fs,t);
 endfor
 
-#plot stuff
-#plottrial(trial,1); #plot trial 1 (aka trial{1,1 through 4}) in both time and frequency domains 
-speakerrangetest(fs, trial, 1, .5, .5, .5, 1, .5); #overlay all trials in frequency domain
-
-#TODO
 #save data to file
+cd trials;
+save(filename);
+cd ..;
 
 #TODO
 #calculate amplitude difference in time domain signals
 
 #TODO
 #calculate usefule amplitude difference in frequency domain signals
+
+#plot a particular frequency trial
+plottrial(trial,1); 
