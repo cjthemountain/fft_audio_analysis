6d6f84477959cbbec59df1b363495464e5642b67
cjthemountain <toothless@localhost.localdomain>
2015-02-19 18:04:23 -0800

    uhhhhhhh, need 2 update comparemeans for whitenoise analysis

--- whitenoise.m
+++ whitenoise.m
@@ -1,27 +1,19 @@
 #white noise (generated gaussain distribution capped at [-1 1]) 
 fprintf(1,"\nwhite noise\n");fflush(1); 
-fprintf("frequency range =  [%f %f]\n",min(frequencies), max(frequencies));fflush(1); 
+fprintf("generating frequency range between [%f %f]\n",min(frequencies), max(frequencies));fflush(1); 
 amplitudes{1}  = wavegen(0,20000,fs,tnoise,'whitenoise'); 
 amplitudes{1} = amplitudes{1}'; 
 times = 1/fs:1/fs:(size(amplitudes{1},2)/fs); 
+fprintf(1,"running trial\n");fflush(1);
 freqdata{1} = performtrial(amplitudes{1},fs); 
-
-#meanamplitude{1} = rms(freqdata{1}{5})*2^.5;
- #GOAL: need to compare mean amplitudes of whitenoise data recorded 
-                                #based on 1/3 octave levels.
-#TODO: average? amplitudes within each octave level
-
-#HOW:? evaluate the FFT of the recorded signal start to end
-#for each value, use the thirdoctaverange function
-	#to sort each point into a list with a member for each list
-	#do IFFT on each member of the list
-	#perform RMS based averaging of each octave amplitude   
-	#dump those values into the meanamplitudes array
 
 for i=1:40
 	sorted{i}=[];
 endfor
 
+fprintf(1,"sorting data by third octave iso standard\n");fflush(1);
+j=0;
+tic;
 for i=1:size(freqdata{1}{4},1) #for each frequency in the fft
 	thisfrequency = freqdata{1}{3}(i);
 	fftdata = freqdata{1}{4}(i);
@@ -30,16 +22,27 @@ for i=1:size(freqdata{1}{4},1) #for each frequency in the fft
 	if z>0
 		sorted{z} = [sorted{z}; freqdata{1}{4}(i)];
 	endif
+	if j==500
+		percentcomplete = i/length(freqdata{1}{4})*100;
+		clc;
+		fprintf(1,"%f%% sorted\t time spent: %f min",percentcomplete, toc/60);fflush(1);
+		j=0;
+	endif
+	j++;
 endfor
 
+fprintf(1,"\nperforming inverse fft\n");fflush(1);
 for i=1:length(sorted)
 	isorted{i} = ifft(sorted{i});
 	meanamplitude{i} = rms(sorted{i})*2^.5;
 endfor
 
+fprintf(1,"calculating means\n");fflush(1);
 for i=1:length(meanamplitude)
 	if length(meanamplitude{i})!=0
 		meanamplitudes(i) = meanamplitude{i};
+		values = thirdoctaverange(meanamplitudes(i));
+		isonumbers(i) = values.isonumber;
 	endif
 endfor
 
@@ -49,6 +52,8 @@ filename = [descriptor "-" mat2str(ztime(1)) "-" mat2str(ztime(2)) "-" mat2str(z
             "-whitenoise" ".m"]; 
  
 plotnumber = plotsummary(frequencies,times,amplitudes,freqdata,fs,croptime,filename, plotnumber); 
+figure();
+plot(isonumbers, meanamplitudes, 'k');
  
 fprintf(1,"saving data to file: %s\n",filename);fflush(1); 
 if exist("./trials", "file")!=7 
@@ -57,3 +62,4 @@ endif
 cd trials; 
 save(filename); 
 cd ..; 
+toc
